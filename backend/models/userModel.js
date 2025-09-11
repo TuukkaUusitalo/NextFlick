41e86a63f8e4e6db79fb15d493a7365a6825039f
long3011 <minhlongvohoang@gmail.com>
2025-09-11 13:20:14 +0300

    added backend base and modifed basic medels

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true }, // Unique username
+    email: { type: String, required: true }, // Unique email address
+    password: { type: String, required: true }, // Hashed password
+    profilePicture: { type: String }, // URL to profile picture 
+    bio: { type: String }, // User biography
+    recommendedMovies: [{type: Schema.Types.ObjectId, ref: 'Blog'}], //Array of blog that have author that is the user
+    watchedMovies: {
+      name: String,
+      movieId: String,
+    }, //Movies the user has watched
+    yetToWatchMovies: {
+      name: String,
+      movieId: String,
+    }, // Movies the user plan to watch
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
